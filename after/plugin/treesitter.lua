a48b054233fa310a85300c410253fe89ff9eea2f
Harrison DiAmbrosio <81518632+EggbertFluffle@users.noreply.github.com>
2023-11-05 09:19:31 -0500

    put GCC as a valid c compiler for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require('nvim-treesitter.install').compilers = { "clang" }
+require('nvim-treesitter.install').compilers = { "gcc", "clang" }
 require('nvim-treesitter.configs').setup({
 	highlight = {
 		enable = true,
