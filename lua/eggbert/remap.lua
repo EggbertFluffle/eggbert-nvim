b2f311e5e2800b4444799fa31820631594325cba
eggbert <haranaitor@gmail.com>
2023-12-04 13:26:18 -0500

    idk what j happnened

--- lua/eggbert/remap.lua
+++ lua/eggbert/remap.lua
@@ -5,3 +5,5 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>mc", ":!make && ./bin/main")
+vim.keymap.set("n", "<leader>cc", ":!make")
+vim.keymap.set("n", "<leader>aoc", ":!aoc")
